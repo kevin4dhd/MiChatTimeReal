263a562c76186483bcac010f3d8a71e204c67387
kevin4dhd <kevin3dhd@gmail.com>
2016-12-13 19:23:24 -0500

    aa

--- app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/ChatActivity.java
+++ app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/ChatActivity.java
@@ -2,12 +2,37 @@ package pe.yt.com.piazzoli.kevin.michattimereal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private EditText eTusuario;
+    private EditText eTcontraseña;
+    private Button bTingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        eTusuario = (EditText) findViewById(R.id.eTusuario);
+        eTcontraseña = (EditText) findViewById(R.id.eTcontraseña);
+
+        bTingresar = (Button) findViewById(R.id.bTingresar);
+
+        bTingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                VerificarLogin(eTusuario.getText().toString().toLowerCase(),eTcontraseña.getText().toString().toLowerCase());
+            }
+        });
     }
+
+    public void VerificarLogin(String user, String password){
+        Toast.makeText(this,"El usuario es: "+user+" y la contraseña es: "+password,Toast.LENGTH_SHORT).show();
+    }
+
 }
