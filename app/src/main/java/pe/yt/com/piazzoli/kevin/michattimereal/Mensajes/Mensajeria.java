d6ae296876ef18bade10072e701bdddc239ed4c0
kevin4dhd <kevin3dhd@gmail.com>
2017-01-11 22:07:34 -0500

    aa

--- app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/Mensajes/Mensajeria.java
+++ app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/Mensajes/Mensajeria.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +22,10 @@ public class Mensajeria extends AppCompatActivity {
 
 
     private RecyclerView rv;
+    private Button bTEnviarMensaje;
+    private EditText eTEscribirMensaje;
     private List<MensajeDeTexto> mensajeDeTextos;
-    private MensajesAdapater adapter;
+    private MensajeriaAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,8 @@ public class Mensajeria extends AppCompatActivity {
         mensajeDeTextos = new ArrayList<>();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        bTEnviarMensaje = (Button) findViewById(R.id.bTenviarMensaje);
+        eTEscribirMensaje = (EditText) findViewById(R.id.eTEsribirMensaje);
 
         rv = (RecyclerView) findViewById(R.id.rvMensajes);
         LinearLayoutManager lm = new LinearLayoutManager(this);
@@ -38,15 +44,31 @@ public class Mensajeria extends AppCompatActivity {
         for(int i=0;i<10;i++){
             MensajeDeTexto mensajeDeTextoAuxiliar = new MensajeDeTexto();
             mensajeDeTextoAuxiliar.setId(""+i);
-            mensajeDeTextoAuxiliar.setMensaje("hola "+i);
+            mensajeDeTextoAuxiliar.setMensaje("emisor asdaskdjask djklasjd asjkldasdasdasd asdas"+i);
             mensajeDeTextoAuxiliar.setTipoMensaje(1);
             mensajeDeTextoAuxiliar.setHoraDelMensaje("10:2"+i);
             mensajeDeTextos.add(mensajeDeTextoAuxiliar);
         }
 
-        adapter = new MensajesAdapater(mensajeDeTextos);
+        for(int i=0;i<10;i++){
+            MensajeDeTexto mensajeDeTextoAuxiliar = new MensajeDeTexto();
+            mensajeDeTextoAuxiliar.setId(""+i);
+            mensajeDeTextoAuxiliar.setMensaje("receptor asdaskdjask djklasjd asjkldasdasdasd asdas"+i);
+            mensajeDeTextoAuxiliar.setTipoMensaje(2);
+            mensajeDeTextoAuxiliar.setHoraDelMensaje("10:2"+i);
+            mensajeDeTextos.add(mensajeDeTextoAuxiliar);
+        }
+
+        adapter = new MensajeriaAdapter(mensajeDeTextos);
         rv.setAdapter(adapter);
 
+
+        bTEnviarMensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateMensaje(eTEscribirMensaje.getText().toString());
+            }
+        });
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,4 +78,16 @@ public class Mensajeria extends AppCompatActivity {
         });
 
     }
+
+    public void CreateMensaje(String mensaje){
+        MensajeDeTexto mensajeDeTextoAuxiliar = new MensajeDeTexto();
+        mensajeDeTextoAuxiliar.setId("0");
+        mensajeDeTextoAuxiliar.setMensaje(mensaje);
+        mensajeDeTextoAuxiliar.setTipoMensaje(1);
+        mensajeDeTextoAuxiliar.setHoraDelMensaje("10:20");
+        mensajeDeTextos.add(mensajeDeTextoAuxiliar);
+        adapter.notifyDataSetChanged();
+        eTEscribirMensaje.setText("");
+    }
+
 }
---
+++ app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/Mensajes/MensajeriaAdapter.java
@@ -0,0 +1,93 @@
+package pe.yt.com.piazzoli.kevin.michattimereal.Mensajes;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pe.yt.com.piazzoli.kevin.michattimereal.R;
+
+/**
+ * Created by Yomaira on 11/01/2017.
+ */
+
+public class MensajeriaAdapter extends RecyclerView.Adapter<MensajeriaAdapter.MensajesViewHolder> {
+
+    private List<MensajeDeTexto> mensajeDeTextos;
+
+    public MensajeriaAdapter(List<MensajeDeTexto> mensajeDeTextos) {
+        this.mensajeDeTextos = mensajeDeTextos;
+    }
+
+    @Override
+    public MensajeriaAdapter.MensajesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_mensajes,parent,false);
+        return new MensajeriaAdapter.MensajesViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(MensajeriaAdapter.MensajesViewHolder holder, int position) {
+
+        RelativeLayout.LayoutParams rl = (RelativeLayout.LayoutParams) holder.cardView.getLayoutParams();
+        FrameLayout.LayoutParams fl = (FrameLayout.LayoutParams) holder.mensajeBG.getLayoutParams();
+
+        LinearLayout.LayoutParams llMensaje = (LinearLayout.LayoutParams) holder.TvMensaje.getLayoutParams();
+        LinearLayout.LayoutParams llHora = (LinearLayout.LayoutParams) holder.TvHora.getLayoutParams();
+
+        if(mensajeDeTextos.get(position).getTipoMensaje()==1){//EMISOR
+            holder.mensajeBG.setBackgroundResource(R.drawable.in_message_bg);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_LEFT,0);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            llMensaje.gravity = Gravity.RIGHT;
+            llHora.gravity = Gravity.RIGHT;
+            fl.gravity = Gravity.RIGHT;
+            holder.TvMensaje.setGravity(Gravity.RIGHT);
+        }else if(mensajeDeTextos.get(position).getTipoMensaje()==2){//RECEPTOR
+            holder.mensajeBG.setBackgroundResource(R.drawable.out_message_bg);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,0);
+            rl.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            llMensaje.gravity = Gravity.LEFT;
+            llHora.gravity = Gravity.LEFT;
+            fl.gravity = Gravity.LEFT;
+            holder.TvMensaje.setGravity(Gravity.LEFT);
+        }
+
+        holder.cardView.setLayoutParams(rl);
+        holder.mensajeBG.setLayoutParams(fl);
+        holder.TvMensaje.setLayoutParams(llMensaje);
+        holder.TvHora.setLayoutParams(llHora);
+
+        holder.TvMensaje.setText(mensajeDeTextos.get(position).getMensaje());
+        holder.TvHora.setText(mensajeDeTextos.get(position).getHoraDelMensaje());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mensajeDeTextos.size();
+    }
+
+    static class MensajesViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cardView;
+        LinearLayout mensajeBG;
+        TextView TvMensaje;
+        TextView TvHora;
+
+        MensajesViewHolder(View itemView){
+            super(itemView);
+            cardView = (CardView) itemView.findViewById(R.id.cvMensaje);
+            mensajeBG = (LinearLayout) itemView.findViewById(R.id.mensajeBG);
+            TvMensaje = (TextView) itemView.findViewById(R.id.msTexto);
+            TvHora = (TextView) itemView.findViewById(R.id.msHora);
+        }
+    }
+}
+
