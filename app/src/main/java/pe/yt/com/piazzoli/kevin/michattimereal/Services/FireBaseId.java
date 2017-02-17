64dbfa24669e94c1209b20025bd5be276debb6c1
kevin <kevin3dhd@gmail.com>
2017-02-17 18:05:22 +0100

    Actualizacion Numero 23

---
+++ app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/Services/FireBaseId.java
@@ -0,0 +1,16 @@
+package pe.yt.com.piazzoli.kevin.michattimereal.Services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by KevinPiazzoli on 09/02/2017.
+ */
+
+public class FireBaseId extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+    }
+
+}
---
+++ app/src/main/java/pe/yt/com/piazzoli/kevin/michattimereal/Services/FireBaseServiceMensajes.java
@@ -0,0 +1,16 @@
+package pe.yt.com.piazzoli.kevin.michattimereal.Services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by KevinPiazzoli on 09/02/2017.
+ */
+
+public class FireBaseServiceMensajes extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
